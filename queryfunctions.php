410825109347afe6ce83fd190867799f8d8fc6c9
ankita <Snowhite780@gmail.com>
2016-02-08 16:53:14 +0900

    Hotel-management
    Projects for CRAFTS

---
+++ queryfunctions.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+function db_connect($HOST,$USER,$PASS,$DB,$PORT)
+{
+	$conn = mysql_connect($HOST . ":" . $PORT , $USER, $PASS);
+	mysql_select_db($DB);
+	return $conn;
+}
+
+function db_close($conn)
+{
+	mysql_close($conn);
+}
+
+//get data from table
+function mkr_query($strsql,$conn)
+{
+	$rs = mysql_query($strsql,$conn);
+	return $rs;
+}
+
+//get number of rows in results sets
+function num_rows($rs)
+{
+	return @mysql_num_rows($rs);
+}
+
+function fetch_array($rs)
+{
+	return mysql_fetch_array($rs);
+}
+
+//fetch object
+function fetch_object($rs)
+{
+	return mysql_fetch_object($rs);
+}
+
+function free_result($rs)
+{
+	@mysql_free_result($rs);
+}
+
+function data_seek($rs,$cnt)
+{
+	@mysql_data_seek($rs, $cnt);
+}
+
+function error()
+{
+	return mysql_error();
+}
+?>
+
+<?php
+	define("HOST", "localhost");
+	define("PORT", 3306);
+	define("USER", "root");
+	define("PASS", "sa");
+	define("DB", "hotelmis");
+
+	/*define("HOST", "mysql4-h");
+	define("PORT", 3306);
+	define("USER", "h172638rw");
+	define("PASS", "hotelmis321");
+	define("DB", "h172638_hotelmis");*/
+?>
